2e15aba05b15fefd54f15efdb7b22d8719babe13
DLillard12 <dannylillard12@gmail.com>
2025-04-02 19:03:29 -0500

    changed dataset to iris as it was easier to classify

--- bayesian_prob/bayesian_prob.md
+++ bayesian_prob/bayesian_prob.md
@@ -3,9 +3,10 @@ I will be implementing a naive bayes classifier on some pokemon data.
 # File Structure
 - Bayesian_prob.md: You are here! This will contain my notes, thoughts, and meta info.
 - bayesian_prob.py: Here I will create a script to calculate bayesian probability and output the stats to a csv.
-- Pokemon.csv: A stripped down version of the dataset from this page: https://www.kaggle.com/datasets/abcsds/pokemon It originally held generations 1-6 but I cut this down to just generation 3.
+- iris.csv: The classic dataset of classifing species of iris.
 # Process
 1. Load data
 2. Define the train test split
-3. Pass train data, test data, and output variable to a function that will return the test data with predictions.
-4. Save that to a csv.
+3. fit the data. We do this by grabbing the priors, means, and variances for each type of each field.
+4. Predict values. We do this by going by each row, then each class, apply the gaussian_pdf to the row and find which class has the best probability
+5. print the true class with the predicted class.
--- bayesian_prob/bayesian_prob.py
+++ bayesian_prob/bayesian_prob.py
@@ -4,46 +4,78 @@
 
 import pandas as pd # just importing pandas to get dataframes, make life easier.
 import numpy
+from collections import defaultdict # using this so I can have 2d dictionaries.
+
 
 # Functions
-def gaussian_pdf(series: pd.Series):
-    var = series.var()
-    mean = series.mean()
+def gaussian_pdf(value, mean, var):
     # Calculate the Gaussian PDF using the formula:
-    numerator = numpy.exp(-((series - mean)**2) / (2 * var))
+    numerator = numpy.exp(-((value - mean)**2) / (2 * var))
     denominator = numpy.sqrt(2 * numpy.pi * var)
 
     return numerator / denominator
 
-def get_bayesian_prob(train, test, target) -> pd.DataFrame:
-    # first, calculate prior from train
-    prior = train[target].value_counts(normalize=True)
-    print(prior)
-    # need to use Gaussian Probability Density Function to
-    # use the numerical fields.
-    numeric_vals = ['Total', 'HP', 'Attack', 'Defense', 'Sp. Atk', 'Sp. Def',
-       'Speed']
-    # need to apply this for each class.
-    groups = list(train['Type 1'].unique())
-    print(groups)
-    # now apply PDF function to each group
-    for group in groups:
-        train.loc[train['Type 1'] == group, numeric_vals] = train.loc[train['Type 1'] == group, numeric_vals].apply(gaussian_pdf)
-    print(train.tail())
+def predict(test_data, type_priors, means, variances):
+    predictions = []
+
+    for i,row in test_data.iterrows():
+        type_probs = defaultdict(dict)
+
+        for type in type_priors:
+            prior = type_priors[type]
+            likelihood = 1  # Start with 1, as we'll multiply PDFs
+
+            numeric_fields = ['sepal_length','sepal_width','petal_length','petal_width']
+            for field in numeric_fields:
+                value = row[field]
+                mean = means[type][field]
+                variance = variances[type][field]
+                
+                # Compute Gaussian PDF
+                pdf = gaussian_pdf(value, mean, variance)
+                likelihood *= pdf  # Multiply all likelihoods
+
+            type_probs[type] = numpy.log(likelihood * prior)  # Multiply by prior
+        #best_class = class with highest probability
+        best_pred = max(type_probs,key=type_probs.get)
+        #append best_class to predictions
+        predictions.append([row['species'], best_pred])
+    return predictions
+
+def fit(train:pd.DataFrame):
+    # initialize empty dictionaries for type_priors, means, and variances
+    type_priors = defaultdict(dict)
+    means = defaultdict(dict)
+    variances = defaultdict(dict)
+
+    types = train['species'].unique()
+
+    
+    for type in types:
+        # subset = dataframe where target_column == class
+        subset = train.loc[train['species'] == type]
+        type_priors[type] = len(subset)/len(train)
+        numeric_fields = ['sepal_length','sepal_width','petal_length','petal_width']
+        for field in numeric_fields:
+            means[type][field] = subset[field].mean()
+            variances[type][field] = subset[field].var()
+
+    return type_priors, means, variances
 
 
 
 # Main here and below.
-user_file_name = "Pokemon.csv"
-target = 'Type 1'
+user_file_name = 'iris.csv'
 
 df = pd.read_csv(user_file_name)
 
-df=df.drop(['Name','Type 2','Generation','Legendary'],axis=1)
 print(df.tail())
 
 # defining the training and test split.
 train=df.sample(frac=0.6,random_state=200)
 test=df.drop(train.index)
 
-df_with_calc = get_bayesian_prob(train, test, target)
+type_priors, means, variances = fit(train)
+preds = predict(test,type_priors,means,variances)
+
+print(preds)
